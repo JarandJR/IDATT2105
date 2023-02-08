1ba05f45229794b510c19805bd6799bc47018118
Jarand Jensen Romestrand <jarandjr@stud.ntnu.no>
2023-02-08 18:16:18 +0100

    Finished most of the form functionality and looks

--- Øvinger/Øving3/CalculatorWithForum/src/components/contactform/ContactForm.vue
+++ Øvinger/Øving3/CalculatorWithForum/src/components/contactform/ContactForm.vue
@@ -3,18 +3,18 @@
     <form @submit.prevent="submitForm">
       <div>
         <label>Full name: </label>
-        <input type="text" placeholder="Name" v-model="name" name="name" required>
+        <input type="text" placeholder="Name" v-model="name" name="name" v-on:keydown="validate" v-on:change="validate" v-on:keyup="validate">
       </div>
       <div>
         <label>E-mail: </label>
-        <input type="text" placeholder="E-mail" v-model="mail" name="mail" required>
+        <input type="text" placeholder="E-mail" v-model="mail" name="mail" v-on:keydown="validate()" v-on:change="validate" v-on:keyup="validate">
       </div>
       <div>
         <label>Message: </label>
-        <textarea placeholder="Message" v-model="message" name="message" required/>
+        <textarea placeholder="Message" v-model="message" name="message" v-on:keydown="validate" @click="validate" v-on:change="validate" v-on:keyup="validate"/>
       </div>
       <div>
-        <button type="submit">Submit</button>
+        <button  type="submit" disabled="disabled" ref="submitBtn">Submit</button>
       </div>
     </form>
   </div>
@@ -31,9 +31,44 @@ export default {
     }
   },
   methods: {
-    async submitForm() {
-      const {name, mail, message} = this;
+    submitForm() {
+      /*
+      * To do!
+      * fix state on mail and name
+      * mock json-server
+      * unit test for calculator and form
+      * scenario tests for contact form
+      * */
+      //this.$store.commit("SET_NAME",this.name);
+      //this.$store.commit("SET_EMAIL",this.mail);
+      //let counter = 0;
+      //console.log(counter);
+      //this.$store.commit("increment", counter);
+      //console.log(counter);
+
+      const { name, mail, message } = this;
       console.log(name + " " + mail + " " + message);
+      //this.name = this.$store.name;
+      //this.mail = this.$store.mail;
+    },
+    validate() {
+      this.$refs["submitBtn"].disabled = !(
+        this.checkMail() &&
+        this.checkName() &&
+        this.checkMessage()
+      );
+    },
+    checkName() {
+      return this.name.split(" ").length > 1;
+    },
+    checkMail() {
+      return this.mail.split("@").length > 1 && this.mail.split(".").length > 1;
+    },
+    checkMessage() {
+      return this.message.length > 0 && this.isEmpty(this.message);
+    },
+    isEmpty(str) {
+      return !str || str.trim().length > 0;
     }
   }
 }
@@ -45,13 +80,17 @@ form {
 }
 
 button {
+  border-radius: 25px;
   width: 100%;
-  height: 30px;
-  font-size: 70%;
+  font-size: 80%;
+  height: 50px;
   text-align: center;
-  background-color: limegreen;
+  background-color: green;
   min-width: 350px;
   padding: 0 10px;
+}
+button:hover:enabled {
+    background-color: limegreen;
 }
 
 input {
