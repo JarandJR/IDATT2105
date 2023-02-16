e87476ca762b788e298637bc26e8f10eaebe1a09
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-02-16 20:44:58 +0100

    initial commit task 4

---
+++ Øvinger/Øving4/CalculatorBackend/src/test/java/stud/ntnu/jarandjr/CalculatorBackend/CalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.jarandjr.CalculatorBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
