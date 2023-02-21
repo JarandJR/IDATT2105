a924d71d896f1153782c1b15cac4353927c7bcec
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-02-21 15:58:52 +0100

    Frontend can now comunicate with backend

---
+++ Øvinger/Øving4/Backend/CalculatorBackend/src/main/java/stud/ntnu/backend/calculator/CalculatorBackend.java
@@ -0,0 +1,13 @@
+package stud.ntnu.backend.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorBackend {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorBackend.class, args);
+    }
+
+}
---
+++ Øvinger/Øving4/Frontend/CalculatorFrontend/src/components/homepage/Greeting.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <h1>Welcome to my app</h1>
+    <p>Greetings from backend: {{connection}}</p>
+    <button @click="testConnection">Test connection</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const connection = ref("Not Connected");
+
+async function testConnection(){
+  connection.value = await (await axios.get("http://127.0.0.1:8080/")).data;
+}
+</script>
+
+<style scoped>
+.wrapper {
+  justify-content: center;
+  padding: 2rem;
+}
+</style>
